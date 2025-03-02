01498836a9a3576c7e953e90a6e50a5ed7d0a51c
Michelle Ogunade <132501606+michelleog2351@users.noreply.github.com>
2025-03-02 21:10:01 +0000

    [WIP] Add files to repo - commencing banking app project

---
+++ Project_Secure Banking Application/src/main/java/com/project/mybankaccount/BankAccount.java
@@ -0,0 +1,7 @@
+package src.main.java.com.project.mybankaccount;
+public class BankAccount
+{
+    public static void main(String[] args) {
+    
+    }
+}
---
+++ Project_Secure Banking Application/src/test/java/com/project/mybankaccount/BankAccountTest.java
@@ -0,0 +1,4 @@
+package src.test.java.com.project.mybankaccount;
+public class BankAccountTest {
+    
+}
