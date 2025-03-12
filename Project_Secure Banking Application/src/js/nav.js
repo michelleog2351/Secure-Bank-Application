c258f5564d2474f89604a422a4d6b84ac8109236
Michelle Ogunade <132501606+michelleog2351@users.noreply.github.com>
2025-03-12 20:06:39 +0000

    add changes to structures in the file

--- Project_Secure Banking Application/src/js/nav.js
+++ Project_Secure Banking Application/src/js/nav.js
@@ -3,26 +3,24 @@ function nav() {
       <div class="container d-flex justify-content-center"> 
           <ul class="navbar-nav" style="padding: 15px 15px 15px 15px">
               <li class="nav-item">
-                  <a class="nav-link" href="index.html">Home</a>
+                  <a class="nav-link" href="../../../index.html">Home</a>
               </li>
 
-              <li class="nav-item">
-                  <a class="nav-link" href="login.html">Login</a>
-              </li>
+             
       `;
-  if (sessionStorage.getItem("login") == "true") {
-    navOutPut += `
-          <li class="nav-item">
-          <a class="nav-link" href="/admin.html">Admin</a>
-          </li> |
-          <li class="nav-item">
-          <a class="nav-link" href="/logout.html">Logout</a>
-          </li> |`;
-  } else {
-    navOutPut += `<li class="nav-item">
-          <a class="nav-link" href="login.html">Login</a> 
-          </li>`;
-  }
+  // if (sessionStorage.getItem("login") == "true") {
+  //   navOutPut += `
+  //         <li class="nav-item">
+  //         <a class="nav-link" href="/admin.html">Admin</a>
+  //         </li> |
+  //         <li class="nav-item">
+  //         <a class="nav-link" href="/logout.html">Logout</a>
+  //         </li> |`;
+  // } else {
+  //   navOutPut += `<li class="nav-item">
+  //         <a class="nav-link" href="login.html">Login</a> 
+  //         </li>`;
+  // }
   navOutPut += `</ul></div>`;
   $("nav").html(navOutPut);
 }
